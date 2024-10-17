411274491dd9cdb1386519db80ac1fd8b2dfb468
ngcthao <lyng@oregonstate.edu>
2024-10-17 16:57:14 -0400

    Ally-Units
    Initial draft of ally unit files and ally unit manager. Edited index.js file to remove player import.

---
+++ src/js/game.js
@@ -0,0 +1,18 @@
+/**
+ * hold all game specific data
+ */
+var game = {
+
+    /**
+     * object where to store game global scole
+     */
+    data : {
+        playerHealth: 100,
+        currency: 100,
+    },
+
+    // a reference to the texture atlas
+    texture : null
+};
+
+export default game;
