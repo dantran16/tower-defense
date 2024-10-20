1b21872fc42c85a8c087858c5921d28994d0695c
Dan Tran <dantranux@gmail.com>
2024-10-20 18:49:35 -0400

    Refactored params. Added a very basic side menu UI.

--- src/js/params.js
+++ src/js/params.js
@@ -1,3 +1,13 @@
 export const params = {
-    buttonBorderSize: 75
+    playButton:{
+        borderSize: 75
+    },
+    towerButton: {
+        borderSize: 30,
+        size: 1/3
+    },
+    pauseButton: {
+        borderSize: 20,
+        size: 1/2
+    }
 }
