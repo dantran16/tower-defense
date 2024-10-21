ee848edb64f0fd2173c874f01ee4af89370e7020
zirtidik <zirtidik@oregonstate.edu>
2024-10-21 17:50:54 -0400

    Added rewards for killing enemy

--- src/js/renderables/enemies/enemy_class.js
+++ src/js/renderables/enemies/enemy_class.js
@@ -1,7 +1,7 @@
 import * as me from 'melonjs';
 
 class Enemy extends me.Entity {
-    constructor(x, y, settings, health, speed, element) {
+    constructor(x, y, settings, health, speed, element, reward) {
         // Call parent constructor to initialize the position and settings
         super(x, y, settings);
 
@@ -9,20 +9,21 @@ class Enemy extends me.Entity {
         this.health = health;         // Health of the enemy
         this.speed = speed;           // Movement speed of the enemy
         this.element = element;       // Type/Element (e.g., fire, water, etc.)
+        this.reward = reward;         // Reward for kill enemy
 
         // Set the velocity for movement
         this.body.setVelocity(this.speed, this.speed); // Movement speed in x and y directions
 
-        // Disable gravity for the enemy (since it's not needed in a tower defense game)
+        // Disable gravity for the enemy
         this.body.gravity = 0;
 
-        // Define the hitbox (adjust based on sprite size)
+        // Define the hitbox 
         this.body.addShape(new me.Rect(0, 0, this.width, this.height));
 
         // Ensure the enemy is always updated, even when off-screen
         this.alwaysUpdate = true;
 
-        // Make the enemy collidable (for interaction with other objects)
+        // Make the enemy collidable 
         this.isCollidable = true;
     }
 
@@ -54,7 +55,19 @@ class Enemy extends me.Entity {
 
         // Remove the enemy from the game world when it dies
         me.game.world.removeChild(this);
+
+        this.rewardPlayer();
+    }
+
+    rewardPlayer(){
+        me.game.world.getChildbyName('player')[0].coins += this.reward;
+
+        console.log(`Player rewarded with ${this.reward} coins.`)
     }
 }
 
 export default Enemy;
+
+// depending on path may need to update movement logic.
+// Will need to add enemy comes off screen and "dies"/ disappears
+// Need to ask if we have coin tracker.
--- src/js/renderables/enemies/enemy_objects.js
+++ src/js/renderables/enemies/enemy_objects.js
@@ -7,9 +7,9 @@ class Level1 extends me.Stage {
         me.game.world.reset();
 
         // Add enemies to the world
-        const onigiriEnemy = new Enemy(100, 50, { width: 32, height: 32, image: 'onigiri_image' }, 50, 4, 'Onigiri');
-        const redVelvetEnemy = new Enemy(200, 50, { width: 32, height: 32, image: 'red_velvet_image' }, 200, 1, 'Red Velvet Cake');
-        const coffeeEnemy = new Enemy(300, 50, { width: 32, height: 32, image: 'coffee_image' }, 100, 2, 'Coffee');
+        const onigiriEnemy = new Enemy(100, 50, { width: 32, height: 32, image: 'onigiri_image' }, 50, 4, 'Onigiri', 1);
+        const redVelvetEnemy = new Enemy(200, 50, { width: 32, height: 32, image: 'red_velvet_image' }, 200, 1, 'Red Velvet Cake', 3);
+        const coffeeEnemy = new Enemy(300, 50, { width: 32, height: 32, image: 'coffee_image' }, 100, 2, 'Coffee', 5);
 
         // Add the enemies to the game world
         me.game.world.addChild(onigiriEnemy);
