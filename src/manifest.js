479b674812e72205cc674d4f38af3ae35f052e7d
PearApple <trand5@oregonstate.edu>
2024-10-24 01:55:27 -0700

    added map as temp png file to verify that path works. added tmx, tsx, png files to maps folder updated manifest.js with map data

--- src/manifest.js
+++ src/manifest.js
@@ -11,7 +11,37 @@ const DataManifest = [
         name: "PressStart2P",
         type: "binary",
         src: "./data/fnt/PressStart2P.fnt"
-    }
+    },
+
+    // {
+    //     name: "map",
+    //     type: "json",
+    //     src: "./src/data/map/map.json"
+    // },
+
+    {
+        name: "map",
+        type: "image",
+        src: "./src/data/map/map.png"
+    },
+
+    // {
+    //     name: "map",
+    //     type: "tmx",
+    //     src: "./data/map/untitled.tmx"
+    // },
+
+    // {
+    //     name: "map_assets",
+    //     type: "tsx",
+    //     src: "./data/map/12_Kitchen_32x32.tsx",
+    // },
+
+    // {
+    //     name: "map_floor",
+    //     type: "tsx",
+    //     src: "./data/map/Room_Builder_Floors_32x32.tsx"
+    // }
 ];
 
 export default DataManifest;
