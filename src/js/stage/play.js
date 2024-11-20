8709cacc40f7435d050c08c56cfb08a89a047491
ngcthao <lyng@oregonstate.edu>
2024-11-19 19:19:35 -0500

    create conveyor belt entity

--- src/js/stage/play.js
+++ src/js/stage/play.js
@@ -3,6 +3,7 @@ import SideMenuContainer from "../renderables/ui/SideMenuContainer";
 import GameHUD from "../renderables/ui/GameHUD";
 import Map from '../renderables/ui/Map';
 import applicationState from '../applicationState';
+import ConveyorBelt from '../renderables/ui/ConveyorBelt';
 //import { ImageLayer } from '../../../node_modules/melonjs/dist/types/index';
 
 
@@ -29,10 +30,23 @@ class PlayScreen extends me.Stage {
         const panel = new SideMenuContainer(width * 5/6, 0, width / 6, height);
         const gameHud = new GameHUD(0, 0, width * 5 / 6, height);
 
+        const belt1 = new ConveyorBelt(784, 187, {width: 64, height: 416})
+        const belt2 = new ConveyorBelt(176, 187, {width: 384, height: 64})
+        const belt3 = new ConveyorBelt(176, 187+64, {width: 64, height: 449})
+        const belt4 = new ConveyorBelt(496, 187+64, {width: 64, height: 352})
+        const belt5 = new ConveyorBelt(496+64, 539, {width: 224, height: 64})
+        
+
         me.game.world.addChild(new me.ColorLayer("background", "#202020"));
         me.game.world.addChild(game_map);
+        me.game.world.addChild(belt1)
+        me.game.world.addChild(belt2)
+        me.game.world.addChild(belt3)
+        me.game.world.addChild(belt4)
+        me.game.world.addChild(belt5)
         me.game.world.addChild(panel);
         me.game.world.addChild(gameHud);
+        
     }
 
 };
