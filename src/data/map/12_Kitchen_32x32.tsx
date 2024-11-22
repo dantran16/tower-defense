2472d0de637d054576e512be45f1d4acbab2ce84
PearApple <trand5@oregonstate.edu>
2024-11-21 22:09:18 -0800

    fixed image source files

--- src/data/map/12_Kitchen_32x32.tsx
+++ src/data/map/12_Kitchen_32x32.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="12_Kitchen_32x32" tilewidth="32" tileheight="32" tilecount="784" columns="16">
- <image source="../../../../../Users/dant9/Downloads/moderninteriors-win/1_Interiors/32x32/Theme_Sorter_32x32" width="512" height="1568"/>
+ <image source="../../../../../Users/dant9/Downloads/moderninteriors-win/1_Interiors/32x32/Theme_Sorter_32x32/12_Kitchen_32x32.png" width="512" height="1568"/>
 </tileset>
--- src/data/map/Room_Builder_Floors_32x32.tsx
+++ src/data/map/Room_Builder_Floors_32x32.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="Room_Builder_Floors_32x32" tilewidth="32" tileheight="32" tilecount="600" columns="15">
- <image source="../../../../../Users/dant9/Downloads/moderninteriors-win/1_Interiors/32x32/Room_Bulder_subfiles_32x32" width="480" height="1280"/>
+ <image source="../../../../../Users/dant9/Downloads/moderninteriors-win/1_Interiors/32x32/Room_Bulder_subfiles_32x32/Room_Builder_Floors_32x32.png" width="480" height="1280"/>
 </tileset>
