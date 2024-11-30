cda29d5db0396b6a436d42fb546e5ca3e5b58244
ngcthao <lyng@oregonstate.edu>
2024-11-30 10:40:25 -0500

    show ally stats

--- src/js/renderables/enemies/waypoint1.js
+++ src/js/renderables/enemies/waypoint1.js
@@ -1,30 +1,30 @@
-const size = 16;
+const size = 8;
 
 const waypoints = [
 
     {
-        "x":799+size,
-        "y":204+size
+        "x":799-size,
+        "y":201
+        }, 
+       {
+        "x":799-size,
+        "y":554-size
        }, 
        {
-        "x":799+size,
-        "y":554+size
+        "x":543-size,
+        "y":554-size
        }, 
        {
-        "x":543+size,
-        "y":554+size
+        "x":543-size,
+        "y":202-size
        }, 
        {
-        "x":543+size,
-        "y":202+size
+        "x":190-size,
+        "y":202-size
        }, 
        {
-        "x":190+size,
-        "y":202+size
-       }, 
-       {
-        "x":190+size,
-        "y":651+size
+        "x":190-size,
+        "y":651-size
        }
     ]
     
